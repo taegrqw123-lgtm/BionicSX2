97dbc9b2766a8a5310da87a799dd7387b886a61e
taegrqw123-lgtm <taegr.qw123@gmail.com>
2026-05-15 01:50:55 +0000

    fix: complete Metal stub with all pure virtual methods, fix PresentResult enum

--- metal/MetalRendererStub.mm
+++ metal/MetalRendererStub.mm
@@ -3,7 +3,6 @@
 // STATUS: STUB — Minimal Metal renderer for initial iOS build
 
 #import <Metal/Metal.h>
-#import <UIKit/UIKit.h>
 #include "PrecompiledHeader.h"
 #include "GS/GS.h"
 #include "GS/Renderers/Common/GSDevice.h"
@@ -13,7 +12,7 @@
 class MetalRendererStub : public GSDevice
 {
 public:
-    MetalRendererStub() : GSDevice(GSRendererType::Metal) {}
+    MetalRendererStub() : GSDevice() {}
     ~MetalRendererStub() override {}
 
     bool Create(GSVSyncMode vsync_mode, bool allow_present_throttle) override
@@ -37,69 +36,41 @@ public:
     void SetVSyncMode(GSVSyncMode mode, bool allow_present_throttle) override {}
     bool SetGPUTimingEnabled(bool enabled) override { return false; }
     float GetAndResetAccumulatedGPUTime() override { return 0.0f; }
-
     void ResizeWindow(u32 width, u32 height, float scale) override {}
 
-    PresentResult BeginPresent(bool frame_skip) override { return PresentResult::FrameSkip; }
+    PresentResult BeginPresent(bool frame_skip) override { return PresentResult::FrameSkipped; }
     void EndPresent() override {}
-
     void ClearSamplerCache() override {}
 
-    std::unique_ptr<GSDownloadTexture> CreateDownloadTexture(u32 width, u32 height, GSTexture::Format format) override
-    {
-        return nullptr;
-    }
+    void PushDebugGroup(const char* fmt, ...) override {}
+    void PopDebugGroup() override {}
+    void InsertDebugMessage(DebugMessageCategory category, const char* fmt, ...) override {}
 
-    GSTexture* CreateSurface(GSTexture::Type type, int width, int height, int levels, GSTexture::Format format) override
-    {
-        return nullptr;
-    }
-
-    void CopyRect(GSTexture* sTex, GSTexture* dTex, const GSVector4i& r, u32 destX, u32 destY) override {}
+    std::unique_ptr<GSDownloadTexture> CreateDownloadTexture(u32, u32, GSTexture::Format) override { return nullptr; }
+    GSTexture* CreateSurface(GSTexture::Type, int, int, int, GSTexture::Format) override { return nullptr; }
+    void CopyRect(GSTexture*, GSTexture*, const GSVector4i&, u32, u32) override {}
 
     void DoMerge(GSTexture* sTex[3], GSVector4* sRect, GSTexture* dTex, GSVector4* dRect,
         const GSRegPMODE& PMODE, const GSRegEXTBUF& EXTBUF, u32 c, const bool linear) override {}
-
-    void DoInterlace(GSTexture* sTex, const GSVector4& sRect, GSTexture* dTex, const GSVector4& dRect,
-        ShaderInterlace shader, bool linear, const InterlaceConstantBuffer& cb) override {}
-
-    void DoFXAA(GSTexture* sTex, GSTexture* dTex) override {}
-    void DoShadeBoost(GSTexture* sTex, GSTexture* dTex, const float params[4]) override {}
-
-    bool DoCAS(GSTexture* sTex, GSTexture* dTex, bool sharpen_only,
-        const std::array<u32, NUM_CAS_CONSTANTS>& constants) override
-    {
-        return false;
-    }
-
-    void RenderHW(GSHWDrawConfig& config) override {}
-
-    void PresentRect(GSTexture* sTex, const GSVector4& sRect, GSTexture* dTex,
-        const GSVector4& dRect, PresentShader shader, float shaderTime, bool linear) override {}
-
-    void DrawMultiStretchRects(const MultiStretchRect* rects, u32 num_rects,
-        GSTexture* dTex, ShaderConvert shader) override {}
-
-    void UpdateCLUTTexture(GSTexture* sTex, float sScale, u32 offsetX, u32 offsetY,
-        GSTexture* dTex, u32 dOffset, u32 dSize) override {}
-
-    void ConvertToIndexedTexture(GSTexture* sTex, float sScale, u32 offsetX, u32 offsetY,
-        u32 SBW, u32 SPSM, GSTexture* dTex, u32 DBW, u32 DPSM) override {}
-
-    void FilteredDownsampleTexture(GSTexture* sTex, GSTexture* dTex, u32 downsample_factor,
-        const GSVector2i& clamp_min, const GSVector4& dRect) override {}
-
-    void BeginDSAsRT(GSTexture* ds, const GSVector4i& drawarea) override {}
-
-    void FlushClears(GSTexture* tex) override {}
+    void DoInterlace(GSTexture*, const GSVector4&, GSTexture*, const GSVector4&,
+        ShaderInterlace, bool, const InterlaceConstantBuffer&) override {}
+    void DoFXAA(GSTexture*, GSTexture*) override {}
+    void DoShadeBoost(GSTexture*, GSTexture*, const float[4]) override {}
+    bool DoCAS(GSTexture*, GSTexture*, bool, const std::array<u32, NUM_CAS_CONSTANTS>&) override { return false; }
+    void RenderHW(GSHWDrawConfig&) override {}
+    void PresentRect(GSTexture*, const GSVector4&, GSTexture*, const GSVector4&,
+        PresentShader, float, bool) override {}
+    void DrawMultiStretchRects(const MultiStretchRect*, u32, GSTexture*, ShaderConvert) override {}
+    void UpdateCLUTTexture(GSTexture*, float, u32, u32, GSTexture*, u32, u32) override {}
+    void ConvertToIndexedTexture(GSTexture*, float, u32, u32, u32, u32, GSTexture*, u32, u32) override {}
+    void FilteredDownsampleTexture(GSTexture*, GSTexture*, u32, const GSVector2i&, const GSVector4&) override {}
+    void BeginDSAsRT(GSTexture*, const GSVector4i&) override {}
 
 protected:
-    void DoStretchRect(GSTexture* sTex, const GSVector4& sRect, GSTexture* dTex,
-        const GSVector4& dRect, GSHWDrawConfig::ColorMaskSelector cms,
-        ShaderConvert shader, bool linear) override {}
+    void DoStretchRect(GSTexture*, const GSVector4&, GSTexture*, const GSVector4&,
+        GSHWDrawConfig::ColorMaskSelector, ShaderConvert, bool) override {}
 };
 
-// Factory function called from GSDevice.cpp
 GSDevice* MakeGSDeviceMTL()
 {
     return new MetalRendererStub();
